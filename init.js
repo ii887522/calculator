b72b4eabceb824f254b20bc2cbefc8002d8024f5
ii887522 <ii887522@gmail.com>
2020-05-21 10:24:38 +0800

    Allow depending on zip files from the internet

--- init.js
+++ init.js
@@ -4,4 +4,6 @@ import { dependencies, zip } from "./scripts/out/init_system.js";
 
 dependencies(() => {
     zip("https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip");
+    zip("https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip");
+    zip("https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip");
 });
