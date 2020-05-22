5a78c0afbbaa4564c5bcd2bdfb63a9890825c1c7
ii887522 <ii887522@gmail.com>
2020-05-22 15:06:42 +0800

    Allow checking whether point and rectangle overlap with each other

--- Calculator/src/main/math_ext.h
+++ Calculator/src/main/math_ext.h
@@ -1,15 +1,26 @@
 #ifndef II887522_CALCULATOR_MATH_EXT_H
 #define II887522_CALCULATOR_MATH_EXT_H
 
+#include "Range.h"
 #include "Point.h"
 #include "Rect.h"
 
 namespace ii887522::Calculator
 {
-	// 
+	constexpr bool isOverlapX(const int x, const Range& xRange)
+	{
+		return x >= xRange.min && x < xRange.max;
+	}
+
+	constexpr bool isOverlapY(const int y, const Range& yRange)
+	{
+		return y >= yRange.min && y < yRange.max;
+	}
+
 	constexpr bool isOverlap(const Point& point, const Rect& rect)
 	{
-		return false;
+		return isOverlapX(point.x, Range{ rect.position.x, rect.position.x + rect.size.w }) &&
+			isOverlapY(point.y, Range{ rect.position.y, rect.position.y + rect.size.h });
 	}
 }
 
--- Calculator/src/test/math_ext.test.cpp
+++ Calculator/src/test/math_ext.test.cpp
@@ -2,16 +2,52 @@
 
 #include "math_ext.test.h"
 #include "../main/math_ext.h"
+#include "../main/Range.h"
 
 namespace ii887522::Calculator
 {
+	constexpr static void testIsOverlapX()
+	{
+		static_assert(!isOverlapX(0, Range{ 1, 3 }));
+		static_assert(isOverlapX(1, Range{ 1, 3 }));
+		static_assert(isOverlapX(2, Range{ 1, 3 }));
+		static_assert(!isOverlapX(3, Range{ 1, 3 }));
+		static_assert(!isOverlapX(4, Range{ 1, 3 }));
+		static_assert(!isOverlapX(0, Range{ 2, 5 }));
+		static_assert(isOverlapX(2, Range{ 2, 5 }));
+		static_assert(isOverlapX(4, Range{ 2, 5 }));
+		static_assert(!isOverlapX(6, Range{ 2, 5 }));
+		static_assert(!isOverlapX(8, Range{ 2, 5 }));
+	}
+
+	constexpr static void testIsOverlapY()
+	{
+		static_assert(!isOverlapY(0, Range{ 1, 3 }));
+		static_assert(isOverlapY(1, Range{ 1, 3 }));
+		static_assert(isOverlapY(2, Range{ 1, 3 }));
+		static_assert(!isOverlapY(3, Range{ 1, 3 }));
+		static_assert(!isOverlapY(4, Range{ 1, 3 }));
+		static_assert(!isOverlapY(0, Range{ 2, 5 }));
+		static_assert(isOverlapY(2, Range{ 2, 5 }));
+		static_assert(isOverlapY(4, Range{ 2, 5 }));
+		static_assert(!isOverlapY(6, Range{ 2, 5 }));
+		static_assert(!isOverlapY(8, Range{ 2, 5 }));
+	}
+
 	constexpr static void testIsOverlap()
 	{
-
+		static_assert(!isOverlap(Point{ 0, 0 }, Rect{ Point{ 1, 1 }, Size{ 3, 3 } }));
+		static_assert(!isOverlap(Point{ 2, 0 }, Rect{ Point{ 1, 1 }, Size{ 3, 3 } }));
+		static_assert(isOverlap(Point{ 2, 2 }, Rect{ Point{ 1, 1 }, Size{ 3, 3 } }));
+		static_assert(!isOverlap(Point{ 0, 0 }, Rect{ Point{ 2, 2 }, Size{ 5, 5 } }));
+		static_assert(!isOverlap(Point{ 2, 0 }, Rect{ Point{ 2, 2 }, Size{ 5, 5 } }));
+		static_assert(isOverlap(Point{ 2, 2 }, Rect{ Point{ 2, 2 }, Size{ 5, 5 } }));
 	}
 
 	void testMathExt()
 	{
+		testIsOverlapX();
+		testIsOverlapY();
 		testIsOverlap();
 	}
 }
