4ba2ae20be0a2f13f6a094d0ce9bc78af0056e58
ii887522 <ii887522@gmail.com>
2020-05-23 10:50:28 +0800

    Let button is a view

--- Calculator/src/main/Button.cpp
+++ Calculator/src/main/Button.cpp
@@ -1,10 +1,48 @@
 #ifndef TEST
 
 #include "Button.h"
+#include "View.h"
 #include <SDL.h>
 
 namespace ii887522::Calculator
 {
+	Button::Button(SDL_Renderer*const renderer, const Rect& rect) : View{ renderer }, viewModel{ rect } { }
+
+	Action Button::reactMouseMotion(const SDL_MouseMotionEvent& motionEvent)
+	{
+		viewModel.reactMouseMotion(Point{ motionEvent.x, motionEvent.y });
+		if (viewModel.isAnimating) return Action::START_ANIMATION;
+		return Action::NONE;
+	}
+
+	Action Button::reactLeftMouseButtonDown(const SDL_MouseButtonEvent&)
+	{
+		viewModel.reactLeftMouseButtonDown();
+		if (viewModel.isAnimating) return Action::START_ANIMATION;
+		return Action::NONE;
+	}
+
+	Action Button::reactLeftMouseButtonUp(const SDL_MouseButtonEvent&)
+	{
+		viewModel.reactLeftMouseButtonUp();
+		if (viewModel.isAnimating) return Action::START_ANIMATION;
+		return Action::NONE;
+	}
+
+	Action Button::reactMouseLeaveWindow(const SDL_WindowEvent&)
+	{
+		viewModel.reactMouseLeaveWindow();
+		if (viewModel.isAnimating) return Action::START_ANIMATION;
+		return Action::NONE;
+	}
+
+	Action Button::step(const unsigned int dt)
+	{
+		viewModel.step(dt);
+		if (viewModel.isAnimating) return Action::NONE;
+		return Action::STOP_ANIMATION;
+	}
+
 	void Button::render()
 	{
 		SDL_SetRenderDrawColor(renderer, static_cast<Uint8>(192u * viewModel.lightness.now), static_cast<Uint8>(192u *
--- Calculator/src/main/Button.h
+++ Calculator/src/main/Button.h
@@ -3,6 +3,7 @@
 #ifndef II887522_CALCULATOR_BUTTON_H
 #define II887522_CALCULATOR_BUTTON_H
 
+#include "View.h"
 #include <SDL.h>
 #include "ButtonModel.h"
 #include "Rect.h"
@@ -12,7 +13,7 @@
 namespace ii887522::Calculator
 {
 	// Not Thread Safe: it must only be used in main thread
-	class Button final
+	class Button final : public View
 	{
 		// remove copy semantics
 		Button(const Button&) = delete;
@@ -22,49 +23,18 @@ namespace ii887522::Calculator
 		Button(Button&&) = delete;
 		Button& operator=(Button&&) = delete;
 
-		SDL_Renderer*const renderer;
 		ButtonModel viewModel;
 
 	public:
 		// Param renderer: it must not be assigned to integer
-		explicit constexpr Button(SDL_Renderer*const renderer, const Rect& rect) : renderer{ renderer }, viewModel{ rect } { }
+		explicit Button(SDL_Renderer*const renderer, const Rect& rect);
 
-		constexpr Action reactMouseMotion(const SDL_MouseMotionEvent& motionEvent)
-		{
-			viewModel.reactMouseMotion(Point{ motionEvent.x, motionEvent.y });
-			if (viewModel.isAnimating) return Action::START_ANIMATION;
-			return Action::NONE;
-		}
-
-		constexpr Action reactLeftMouseButtonDown(const SDL_MouseButtonEvent&)
-		{
-			viewModel.reactLeftMouseButtonDown();
-			if (viewModel.isAnimating) return Action::START_ANIMATION;
-			return Action::NONE;
-		}
-
-		constexpr Action reactLeftMouseButtonUp(const SDL_MouseButtonEvent&)
-		{
-			viewModel.reactLeftMouseButtonUp();
-			if (viewModel.isAnimating) return Action::START_ANIMATION;
-			return Action::NONE;
-		}
-
-		constexpr Action reactMouseLeaveWindow(const SDL_WindowEvent&)
-		{
-			viewModel.reactMouseLeaveWindow();
-			if (viewModel.isAnimating) return Action::START_ANIMATION;
-			return Action::NONE;
-		}
-
-		constexpr Action step(const unsigned int dt)
-		{
-			viewModel.step(dt);
-			if (viewModel.isAnimating) return Action::NONE;
-			return Action::STOP_ANIMATION;
-		}
-
-		void render();
+		virtual Action reactMouseMotion(const SDL_MouseMotionEvent&) override;
+		virtual Action reactLeftMouseButtonDown(const SDL_MouseButtonEvent&) override;
+		virtual Action reactLeftMouseButtonUp(const SDL_MouseButtonEvent&) override;
+		virtual Action reactMouseLeaveWindow(const SDL_WindowEvent&) override;
+		virtual Action step(const unsigned int dt) override;
+		virtual void render() override;
 	};
 }
 
